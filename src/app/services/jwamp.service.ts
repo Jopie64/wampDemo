45df95be68b35ac10b187f08cb72ed168ce60975
Johan 't Hart <johan@jdm.nl>
2017-08-24 23:55:01 +0200

    Support connecting to different wamp server

--- src/app/services/jwamp.service.ts
+++ src/app/services/jwamp.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
 import { makeJWamp, JWampProxy } from './jwamp';
-import { Subscription } from 'rxjs/Rx';
+import { Subscription, Subject } from 'rxjs/Rx';
 
 @Injectable()
 export class JWampService {
   public jwamp: JWampProxy;
   public url = 'ws://40.86.85.83:443/ws';
   public realm = 'realm1';
+  public onNewWampData$ = new Subject();
 
   private conns = new Subscription();
 
   constructor() {
+    this.onNewWampData$.asObservable()
+      .map(() => ({url: this.url, realm: this.realm}))
+      .switchMap(v => makeJWamp(v.url, v.realm)
+        .retryWhen(v => v.timeout(2000)))
+      .map(v => v.makeProxy('', false))
+      .subscribe(v => this.jwamp = v);
     this.setWampData(this.url, this.realm);
   }
 
   setWampData(url: string, realm: string)  {
     this.url = url;
     this.realm = realm;
-    this.conns.add(makeJWamp(url, realm)
-      .retryWhen(v => v.timeout(2000))
-      .map(v => v.makeProxy('', false))
-      .subscribe(v => this.jwamp = v));
+    this.onNewWampData$.next();
   }
 }

d6b4a5d62918228c5fc2a555de2abef95cf08e47
Johan 't Hart <johan@jdm.nl>
2017-08-26 00:51:10 +0200

    Connect whisper outputs to inputs

--- src/app/whisper/whisper.component.ts
+++ src/app/whisper/whisper.component.ts
@@ -6,6 +6,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 interface IO {
   rpc: string;
   input$: Observable<string>;
+  onOutput(output: string): void;
 }
 
 @Component({
@@ -17,7 +18,8 @@ export class WhisperComponent implements OnInit {
   public whisperInput = '';
   public whisper$ = new Subject<string>();
   public currentWhisper$: Observable<string>;
-  public ios: string[] = ['Hello', 'World'];
+  public ios: IO[] = [];
+  public finalOutput = '';
 
   constructor(public wampsvc: JWampService) {
     this.currentWhisper$ = this.whisper$.asObservable()
@@ -25,6 +27,22 @@ export class WhisperComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.addIo('Hello');
+    this.addIo('World');
+  }
+
+  addIo(rpc: string) {
+    let input$ = this.currentWhisper$;
+    if (this.ios.length > 0) {
+      const inputSubject = new Subject<string>();
+      this.ios[this.ios.length - 1].onOutput = v => inputSubject.next(v);
+      input$ = inputSubject.asObservable();
+    }
+    this.ios.push({
+      rpc: rpc,
+      input$: input$,
+      onOutput: v => this.finalOutput = v
+    });
   }
 
   onWhisperChange(v: string) {

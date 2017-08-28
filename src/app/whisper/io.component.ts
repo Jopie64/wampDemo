abbc06b6d927129864aee559c5baa70ab246bb7f
Johan 't Hart <johan@jdm.nl>
2017-08-28 10:15:05 +0200

    Remove some debug logging

--- src/app/whisper/io.component.ts
+++ src/app/whisper/io.component.ts
@@ -2,8 +2,6 @@ import { JWampService } from '../services/jwamp.service';
 import { Observable, Subscription, BehaviorSubject, Subject } from 'rxjs/Rx';
 import { Component, Injectable, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
-const logit = name => val => console.log(name + ':', val);
-
 @Component({
   selector: 'app-whisperio',
   templateUrl: './io.component.html'
@@ -29,15 +27,11 @@ export class WhisperIoComponent implements OnInit {
         this.wampsvc.jwamp$,
         (input, wamp) => ({input: input, wamp: wamp}))
       .flatMap(v => v.wamp.callProgress(this.rpc, { argsList: [v.input] })
-          .do(logit(this.rpc + '-callresult1'), logit(this.rpc + '-callresult1 error'))
-          .map(out => out.argsList[0] as string)
-          .do(logit(this.rpc + '-callresult2'), logit(this.rpc + '-callresult2 error'), () => logit(this.rpc + '-result2')('complete'))
-          .do(
-            out => this.output = out,
-            e => this.output = 'Error: ' + e.error)
-          .catch(e => [v.input])
-          .do(logit(this.rpc + '-catch')))
-      .do(logit('finalresult'), logit('finalresultError'))
+        .map(out => out.argsList[0] as string)
+        .do(
+          out => this.output = out,
+          e => this.output = 'Error: ' + e.error)
+        .catch(e => [v.input]))
       .subscribe(v => this.onOutput.emit(v));
   }
 }
